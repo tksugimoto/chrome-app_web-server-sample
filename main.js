e847a22ed02e0442a7f044b50be1114c6bbf410c
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2015-07-19 17:32:27 +0900

    コード整理

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 ﻿
-var link = document.getElementById("link");
 chrome.storage.local.get(["host", "port"], function (items){
 	console.log(items);
 	if (items.host && items.port) {
+		var link = document.getElementById("link");
 		link.href = "http://" + items.host + ":" + items.port;
 		link.style.display = "block";
 	}
